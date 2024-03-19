81a4c83239eb34f6707e018ccb6caeec4e2dd4b5
James Diffey <139918141+jamesdiffeycoding@users.noreply.github.com>
2024-03-19 16:18:53 +0000

    Update index.ts

--- index.ts
+++ index.ts
@@ -14,40 +14,40 @@ const resolvers = {
         reviews() {
             return db.reviews
         },
-        review(_, args, context) {
+        review(_: any, args: any, context: any) {
             return db.reviews.find((review)=> review.id === args.id)
         },
-        author(_, args, context) {
+        author(_: any, args: any, context: any) {
             return db.authors.find((author)=> author.id === args.id)
         },
-        game(_, args, context) {
+        game(_: any, args: any, context: any) {
             return db.games.find((game)=> game.id === args.id)
         }
     },
     Game: {
-        reviews(parent) {
+        reviews(parent: any) {
             return db.reviews.filter((r) => r.game_id === parent.id)
         }
     },
     Author: {
-        reviews(parent) {
+        reviews(parent: any) {
             return db.reviews.filter((r) => r.author_id === parent.id)
         }
     },
     Review: {
-        author(parent) {
+        author(parent: any) {
             return db.authors.find((a) => a.id === parent.author_id)
         },
-        game(parent) {
+        game(parent: any) {
             return db.games.find((g) => g.id === parent.game_id)
         }
     },
     Mutation: {
-        deleteGame(_, args) {
+        deleteGame(_: any, args) {
             db.games = db.games.filter((g) => g.id !== args.id)
             return db.games
         },
-        addGame(_, args) {
+        addGame(_: any, args) {
             let game = {
                 ...args.game, 
                 id: Math.floor(Math.random() * 10000).toString()
